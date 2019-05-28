ebdc18e88dc2b794e5921d6e32a1c96064bab7d9
dIJKLMstra <1500012917@pku.edu.cn>
2019-05-28 15:50:35 -0300

    dual model v5
    add pos tagging into dual model , the dimension of pos tagging embedding layer should not be huge.

--- code/dual_model2.py
+++ code/dual_model2.py
@@ -1,3 +1,9 @@
+'''
+    @AUTHOR QI SUN
+    @DESC   CREATE A MODEL TO CLASSIFY DUAL SENTENCES
+    AND TRY NOT AS STUPID AS OUR FIRST MODEL
+'''
+
 import os
 import pickle
 import numpy as np
@@ -12,16 +18,27 @@ dev_data = []
 test_data = []
 # dictionary used for saving character level embeddings
 embed_dict = {}
-
-# parameters setting
-EMBEDDING_DIM = 601
+# dictionary used for pos taggings
+word_to_ix = {}
+# parameters settings
+WORD_EMBEDDING_DIM = 300
+POS_EMBEDDING_DIM = 50
 HIDDEN_DIM = 100
 SEQUENCE_LENGTH_MAX = 25    # the longest length of sequence using for padding
 PREDICTION_SIZE = 2
+LSTM_LAYER_SIZE = 1
+DROPOUT_RATE = 0.5
 
 pickle_path = '../data/clf_pickles'
 result_path = '../data/result'
 
+def indice_tensor(seq, to_ix):
+    '''
+        get the indices of a given sentence and return its tensor
+    '''
+
+    idxs = [to_ix[w] for w in seq]
+    return torch.tensor(idxs, dtype=torch.long)
 
 def prepare_sequence(word1, word2, to_vec):
     '''
@@ -37,20 +54,23 @@ def prepare_sequence(word1, word2, to_vec):
     vec = np.append(vec, vec_dot)
     return vec
 
-def fill_datalist(lines, data_list):
+def fill_datalist(lines, posLines, data_list):
     '''
         get sentences' embedding and label in different dataset
     '''
 
-    for line in lines:
+    for lidx, line in enumerate(lines):
         info = line[:-1].split('\t')
+        posInfo = posLines[lidx].split('\t')
         if len(info[0]) != len(info[1]):
             continue
         idx_ts = [prepare_sequence(info[0][idx], info[1][idx], embed_dict)\
                   for idx in range(len(info[0]))]
         idx_ts = torch.tensor(idx_ts, dtype=torch.float)
+        pos_idx_ts1 = indice_tensor(posInfo[0], word_to_ix)
+        pos_idx_ts2 = indice_tensor(posInfo[1], word_to_ix)
         label = torch.tensor([int(info[-1])], dtype=torch.long)
-        data_list.append([idx_ts, label])
+        data_list.append([idx_ts, pos_idx_ts1, pos_idx_ts2, label])
 
 def prepare_data():
     '''
@@ -62,51 +82,77 @@ def prepare_data():
     trainPath = '../data/processing_data/dual_train_divided2.tsv'
     devPath = '../data/processing_data/dual_dev_divided2.tsv'
     testPath = '../data/processing_data/dual_test_divided2.tsv'
+    trainPOS = '../data/processing_data/dual_train_POS.txt'
+    devPOS = '../data/processing_data/dual_dev_POS.txt'
+    testPOS = '../data/processing_data/dual_test_POS.txt'
 
     with open(trainPath, 'r', encoding='utf-8') as trainF:
         trainLines = trainF.readlines()
 
-    with open(trainPath, 'r', encoding='utf-8') as devF:
+    with open(devPath, 'r', encoding='utf-8') as devF:
         devLines = devF.readlines()
 
     with open(testPath, 'r', encoding='utf-8') as testF:
         testLines = testF.readlines()
 
+    with open(trainPOS, 'r', encoding='utf-8') as trainP:
+        trainPosLines = trainP.readlines()
+
+    with open(devPOS, 'r', encoding='utf-8') as devP:
+        devPosLines = devP.readlines()
+    
+    with open(testPOS, 'r', encoding='utf-8') as testP:
+        testPosLines = testP.readlines()
+
     with open(os.path.join(pickle_path, \
         'embed_dict.pkl'), 'rb') as ed:
         embed_dict = pickle.load(ed)
     
-    fill_datalist(trainLines, training_data)
-    fill_datalist(devLines, dev_data)
-    fill_datalist(testLines, test_data)
+    POSLines = trainPosLines + devPosLines + testPosLines
+    for line in POSLines:
+        info = line.split('\t')
+        for word in info[0] + info[1]:
+            if word not in word_to_ix:
+                word_to_ix[word] = len(word_to_ix)
+
+    fill_datalist(trainLines, trainPosLines, training_data)
+    fill_datalist(devLines, devPosLines, dev_data)
+    fill_datalist(testLines, testPosLines, test_data)
    
     
 class dualLSTM(nn.Module):
 
-    def __init__(self, embedding_dim, hidden_dim, sentence_size, pred_size):
+    def __init__(self, word_embedding_dim, hidden_dim, dropout_rate, \
+                 layer_size, pos_embedding_dim, vocab_size, pred_size):
         super(dualLSTM, self).__init__()
         self.hidden_dim = hidden_dim
-        self.dropout_rate = 0.5
-        self.layer_size = 2
-        #self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.dropout_rate = dropout_rate
+        self.layer_size = layer_size
 
+        # embedding for pos tagging
+        self.pos_embedding_dim = pos_embedding_dim
+        self.word_embeddings = nn.Embedding(vocab_size, pos_embedding_dim)
+        self.embedding_dim = 2*word_embedding_dim + 2*pos_embedding_dim + 1
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
-
-        self.lstm1 = nn.LSTM(embedding_dim, hidden_dim, self.layer_size,\
-                            dropout=self.dropout_rate, bidirectional=True)
+        self.lstm1 = nn.LSTM(self.embedding_dim, hidden_dim, self.layer_size,\
+            bidirectional=True)
         #self.lstm2 = nn.LSTM(embedding_dim, hidden_dim)
         #self.lstm3 = nn.LSTM(2*hidden_dim, hidden_dim)
 
         # The linear layer that maps from hidden state space to tag space
-        self.vec2pred = nn.Linear(2 * self.layer_size * hidden_dim, pred_size)
+        self.vec2pred = nn.Linear(2*self.layer_size*hidden_dim, pred_size)
         # set a dropout layer for linear layer
         self.dropout = nn.Dropout(self.dropout_rate)
 
-    def forward(self, embeds):
-        #embeds1 = self.word_embeddings(sentence1)
-        #embeds2 = self.word_embeddings(sentence2)
-
+    def forward(self, embeds, pos1, pos2):
+        embeds1 = self.word_embeddings(pos1)
+        embeds2 = self.word_embeddings(pos2)
+        
+        embeds= [torch.cat((embeds[i], embeds1[i], embeds2[i]), 0) \
+                for i in range(len(embeds))]
+        embeds = torch.stack(embeds)
+        embeds = embeds.view(embeds.shape[0], 1, -1)
         #packed_input1 = pack_padded_sequence(embeds1, sent_len)
         #packed_output1, (ht1, ct1) = self.lstm1(packed_input1)
         #output1, _ = pad_packed_sequence(packed_output1)
@@ -135,13 +181,13 @@ def train():
         training part
     '''
 
-    model = dualLSTM(EMBEDDING_DIM, HIDDEN_DIM, \
-        SEQUENCE_LENGTH_MAX, PREDICTION_SIZE)
+    model = dualLSTM(WORD_EMBEDDING_DIM, HIDDEN_DIM, DROPOUT_RATE,\
+                     LSTM_LAYER_SIZE, POS_EMBEDDING_DIM, len(word_to_ix), PREDICTION_SIZE)
     loss_function = nn.NLLLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.1)
 
     dev_accuracy = 0
-    for epoch in range(10):  
+    for epoch in range(20):  
         print('*****epoch ',epoch, '******')
         total_loss = 0
         model.train()
@@ -151,13 +197,13 @@ def train():
             model.zero_grad()
 
             # Step 2. pass our data into our model
-            data[0] = data[0].view(data[0].shape[0], 1, -1)
+            #data[0] = data[0].view(data[0].shape[0], 1, -1)
             #data[1] = data[1].view(SEQUENCE_LENGTH_MAX, 1, -1)
-            prediction = model(data[0])
+            prediction = model(data[0],data[1],data[2])
 
             # Step 3. Compute the loss, gradients, and update the parameters by
             #  calling optimizer.step()
-            loss = loss_function(prediction, data[1])
+            loss = loss_function(prediction, data[3])
             total_loss += loss
             loss.backward()
             optimizer.step()
@@ -173,16 +219,16 @@ def train():
         FN = 0
         total_cnt = 0
         for data in dev_data:
-            data[0] = data[0].view(data[0].shape[0], 1, -1)
-            prediction = model(data[0])
+            #data[0] = data[0].view(data[0].shape[0], 1, -1)
+            prediction = model(data[0],data[1],data[2])
             prediction = torch.argmax(prediction, dim=1)
             zero_tensor = torch.tensor([0], dtype=torch.long)
             one_tensor = torch.tensor([1], dtype=torch.long)
-            if  prediction == zero_tensor and data[1] == zero_tensor:
+            if  prediction == zero_tensor and data[3] == zero_tensor:
                 TN += 1
-            elif prediction == zero_tensor and data[1] == one_tensor:
+            elif prediction == zero_tensor and data[3] == one_tensor:
                 FN += 1
-            elif prediction == one_tensor and data[1] == zero_tensor:
+            elif prediction == one_tensor and data[3] == zero_tensor:
                 FP += 1
             else:
                 TP += 1
@@ -192,15 +238,15 @@ def train():
         if accuracy > dev_accuracy:
             dev_accuracy = accuracy
             with open(os.path.join(pickle_path, \
-                'dual_model_lstm_dropout.pkl'), 'wb') as dm:
+                'dual_model_lstm_pos.pkl'), 'wb') as dm:
                 pickle.dump(model, dm)
 
     with open(os.path.join(pickle_path, \
-        'dual_model_lstm_dropout.pkl'), 'rb') as dm:
+        'dual_model_lstm_pos.pkl'), 'rb') as dm:
         model = pickle.load(dm)
 
     model.eval()
-    resultFile_path = os.path.join(result_path,'dual_lstm_dropout_results.txt')
+    resultFile_path = os.path.join(result_path,'dual_lstm_pos_results.txt')
     with open(resultFile_path, 'w') as writeF:
         for dataset in [training_data, test_data]:
             TP = 0
@@ -209,20 +255,20 @@ def train():
             FN = 0
             total_cnt = 0
             for data in dataset:
-                data[0] = data[0].view(data[0].shape[0], 1, -1)
+                #data[0] = data[0].view(data[0].shape[0], 1, -1)
                 #data[1] = data[1].view(SEQUENCE_LENGTH_MAX, 1, -1)
-                prediction = model(data[0])
+                prediction = model(data[0],data[1],data[2])
                 prediction = torch.argmax(prediction, dim=1)
                 #target = targets.numpy()
                 #prediction = prediction.numpy()
                 zero_tensor = torch.tensor([0], dtype=torch.long)
                 one_tensor = torch.tensor([1], dtype=torch.long)
                 #for idx in range(len(target)):
-                if  prediction == zero_tensor and data[1] == zero_tensor:
+                if  prediction == zero_tensor and data[3] == zero_tensor:
                     TN += 1
-                elif prediction == zero_tensor and data[1] == one_tensor:
+                elif prediction == zero_tensor and data[3] == one_tensor:
                     FN += 1
-                elif prediction == one_tensor and data[1] == zero_tensor:
+                elif prediction == one_tensor and data[3] == zero_tensor:
                     FP += 1
                 else:
                     TP += 1
